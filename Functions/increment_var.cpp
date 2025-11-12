8d83a82f68c88027385008b75e6d8136091d746e
Aymane Akherraz <aakherra@c2r8p4.1337.ma>
2025-11-12 18:20:01 +0100

    Added a new file

---
+++ Functions/increment_var.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void	increment(int &var, int value = 1)
+{
+	var += value;
+}
+
+int main(void) {
+	int var = 0;
+	for(int i = 0; i < 10; i++)
+		if(i % 2)
+			increment(var);
+		else
+			increment(var,i);
+	cout << var << endl;
+	return 0;
+}
