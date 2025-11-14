a7f67cb28da88cf1223a35cba7dd7a1f06f569b2
Aymane Akherraz <aymaneakherraz@gmail.com>
2025-11-14 16:31:13 +0100

    modified is_pangram.cpp

--- Strings/is_pangram.cpp
+++ Strings/is_pangram.cpp
@@ -5,17 +5,27 @@
 
 using namespace std;
 
+bool	is_pangram(const string &s, map<char,int> &char_count)
+{
+	for (char c : s) {
+		if (isalpha(c))
+			char_count[tolower(c)]++;
+	}
+	if (char_count.size() == 26)
+		return true;
+	return false;
+}
+
+int main()
+{
 	string input;
-	int idx;
-	bool is_pangram = true;
-	int letter_count[26] = {};
 	getline(cin, input);
-	// insert code here
-	if (is_pangram)
+	map<char, int> char_count;
+	if (is_pangram(input, char_count))
 		cout << "Pangram" << endl;
 	else
 		cout << "Not Pangram" << endl;
-	for (int i = 'a'; i <= 'z'; i++)
-		cout << char(i) << '-' << letter_count[i - 'a'] << endl;
+	for (char c = 'a'; c <= 'z'; c++)
+		cout << c << '-' << char_count[c] << endl;
 	return 0;
 }
