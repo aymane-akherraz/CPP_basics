2726c41458dbb722b03dfc7dfc07240621587327
Aymane Akherraz <aakherra@c2r7p5.1337.ma>
2025-11-17 19:39:15 +0100

    Modified pattern_match.cpp

--- Strings/pattern_match.cpp
+++ Strings/pattern_match.cpp
@@ -1,5 +1,21 @@
 #include <string>
 #include <iostream>
+#include <cctype>
+
+bool check_type(char c, char type)
+{
+	if (type == 'D')
+		return isdigit(c);
+	if (type == 'A')
+		return isalpha(c);
+	if (islower(type))
+			return (type == tolower(c));
+	if (type == '?')
+		return true;
+	if (ispunct(type))	
+		return (type == c);
+	return (false);
+}
 
 int main()
 {
@@ -8,9 +24,22 @@ int main()
 
     std::string sentence;
     std::getline(std::cin, sentence);
-	for (char c : pattern) {
-			if (c == 'D')
-			
+	std::string match;
+    size_t len = sentence.length();
+	size_t p_len = pattern.length();
+    for (int i = 0; i < len; i++) {
+	    for (int j = 0; j < p_len && i < len; j++) {
+			char c = pattern[j];
+			if (!check_type(sentence[i], c)) {
+				match.clear();
+				break;
+			}
+			else
+				match.push_back(sentence[i++]);
+		}
+		if (match.length() == p_len) {
+			std::cout << match << std::endl;
+			match.clear();
+		}
 	}
-    std::cout << sentence << "\n";
 }
